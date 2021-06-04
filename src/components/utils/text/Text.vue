1e7197641bf7eb48aa22b468027a0b71e686e2f3
Marc Schroeder <marc@stamen.com>
2021-06-03 17:20:33 -0700

    basic working scaleToFit

---
+++ src/components/utils/text/Text.vue
@@ -0,0 +1,72 @@
+<template>
+  <!-- Dynamic text componet adapted from: https://github.com/jens-ox/vue-data-components/tree/master/src/components/text -->
+  <g :transform="`translate(${x},${y}),rotate(${rotate})`">
+    <text
+      v-wrapper="{
+        text,
+        width,
+        align,
+        lineHeight,
+        padding,
+        paddingLeft,
+        paddingRight,
+        scaleToFit
+      }"
+      :dx="dx"
+      :dy="dy"
+      :style="_textStyle"
+    />
+  </g>
+</template>
+<script>
+import wrapper from './textWrap'
+import deepmerge from 'deepmerge'
+
+export default {
+  name: 'DynamicText',
+  directives: { wrapper },
+  props: {
+    text: { type: [String, Number], required: true },
+    x: { type: Number, default: 0 },
+    y: { type: Number, default: 0 },
+    dx: { type: Number, default: 0 },
+    dy: { type: Number, default: 0 },
+    rotate: { type: [Number, String], default: 0 },
+    textStyle: {
+      type: Object,
+      default: () => ({})
+    },
+    width: { type: [Number, String], default: null },
+    align: {
+      type: String,
+      default: 'baseline',
+      validator: value =>
+        ['top', 'baseline', 'bottom', 'middle'].includes(value)
+    },
+    textAnchor: {
+      type: String,
+      default: 'start',
+      validator: value => ['start', 'middle', 'end'].includes(value)
+    },
+    lineHeight: {
+      type: [String, Number],
+      default: '1.125em'
+    },
+    padding: { type: Number, default: 0 },
+    paddingLeft: { type: Number, default: 0 },
+    paddingRight: { type: Number, default: 0 },
+    scaleToFit: { type: Boolean, default: false }
+  },
+  computed: {
+    _textStyle() {
+      return deepmerge(
+        {
+          fill: 'black',
+          'text-anchor': this.textAnchor ?? 'start'
+        },
+        this.textStyle
+      )
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,94 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="test-chart-container">
+    <h2 class="chart-title">test</h2>
+    <svg id="svg-test">
+      <g
+        v-if="domLoaded"
+        :style="{
+          transform: `translate(${this.margin.left}px,${this.margin.top}px)`
+        }"
+      >
+        <rect
+          x="0"
+          y="0"
+          :width="boxWidth"
+          height="300"
+          fill="white"
+          stroke="blue"
+          stroke-width="2"
+        />
+        <DynamicText
+          text="This is the dynamic text. Unless I set a width, it will also run long lonfasfg over where I need it to go."
+          :width="boxWidth"
+          :x="0"
+          :y="15"
+          :textStyle="{
+            fontWeight: 400
+          }"
+          align="baseline"
+          :scaleToFit="true"
+        />
+      </g>
+    </svg>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import * as d3 from 'd3'
+import DynamicText from '@/components/utils/text/Text'
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: 'test',
+  components: { DynamicText },
+  data: function() {
+    return {
+      margin: { top: 15, right: 15, bottom: 0, left: 15 },
+      boxWidth: 650,
+      domLoaded: false
+    }
+  },
+  computed: {
+    width: function() {
+      return (
+        +d3
+          .select('#svg-test')
+          .style('width')
+          .replace('px', '') -
+        this.margin.left -
+        this.margin.right
+      )
+    },
+    height: function() {
+      return (
+        +d3
+          .select('#svg-test')
+          .style('height')
+          .replace('px', '') -
+        this.margin.top -
+        this.margin.bottom
+      )
+    }
+  },
+  mounted() {
+    this.domLoaded = true
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.test-chart-container {
+  width: 800px;
+  height: 500px;
+  display: flex;
+  flex-flow: column;
+
+  .chart-title {
+    flex: 0 0 auto;
+  }
+
+  #svg-test {
+    border: 1px solid rgb(207, 207, 207);
+    width: 100%;
+    flex: 1 1 auto;
+  }
+}
+</style>
